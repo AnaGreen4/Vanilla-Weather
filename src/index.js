ab9dbe0663ac48491582792cebe03ddaba2d9d06
Briana <analikesu@gmail.com>
2023-02-08 22:04:18 -0500

    Added Unit Conversion
    created Celsius temperature conversions and unit F°\ C°
    conversions.

--- src/index.js
+++ src/index.js
@@ -43,6 +43,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#windspeed");
   let iconElement = document.querySelector("#icon");
 
+  celsiusTemp = response.data.main.temp;
   windElement.innerHTML = response.data.wind.speed;
   humidElement.innerHTML = response.data.main.humidity;
   tempElement.innerHTML = temp;
@@ -67,5 +68,29 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+}
+let celsiusTemp = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
